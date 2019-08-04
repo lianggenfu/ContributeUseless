b4752b2ebb2b0ce31ecc1279b82604fc118092ce
eather_liang <917055347@qq.comm>
2019-08-04 23:35:17 +0800

    add3

--- com.example.contribute/test.java
+++ com.example.contribute/test.java
@@ -2,5 +2,6 @@ public class test{
     public static void main(String[] args) {
         System.out.println("sdfsdf");
         System.out.println("contribute出问题了");
+        System.out.println("contribute出问题了");
     }
 }
